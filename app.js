1e533b233f0b62e31db2c3f3b0c819b5ae8a00c8
mohamed-ahmed <mohamed.ahmed@live.ca>
2013-12-02 20:31:02 +0000

    prototyping the app

--- app.js
+++ app.js
@@ -30,8 +30,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+//routes
+
+//load main page
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
